e61ea8226c11556a5dd60ccd705b4c03049c4a27
omprakash8788 <omprakashkumar552@gmail.com>
2025-11-15 00:27:36 +0530

    feat:adding enum rules

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,13 +7,11 @@ const Product = require("../models/Product");
 const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res
-      .status(200)
-      .json({
-        success: true,
-        data: products,
-        message: "Products fetched successfully",
-      });
+    res.status(200).json({
+      success: true,
+      data: products,
+      message: "Products fetched successfully",
+    });
   } catch (error) {
     res.status(500).json({ success: false, message: "Server Error" });
   }
@@ -22,30 +20,27 @@ const getAllProducts = async (req, res) => {
 //@desc Create a new product
 //@route POST /api/products
 //@access Public
-const createProduct=async(req, res)=>{
-    try {
-        const {name, price}=req.body;
-        if(!name || !price){
-            return res.status(400).json({
-                success:false,
-                message:"Name and price are required"
-            })
-        }
-        const newProduct= await Product.create({name, price});
-        res.status(201).json({
-            success:true,
-            data:newProduct,
-            message:"Product created successfully"
-        })
-        
-    } catch (error) {
-        res.status(500).json({success:false, message:"Server Error"})
-        
+const createProduct = async (req, res) => {
+  try {
+    const { name, price, category, tags } = req.body;
+    if (!name || !price || !category || !tags) {
+      return res.status(400).json({
+        success: false,
+        message: "Name and price are required",
+      });
     }
-}
-
+    const newProduct = await Product.create({ name, price, category, tags });
+    res.status(201).json({
+      success: true,
+      data: newProduct,
+      message: "Product created successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
 
 module.exports = {
   getAllProducts,
-  createProduct
+  createProduct,
 };
