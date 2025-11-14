27f0d85b8e5b7843da10c8dde582698949a975ce
omprakash8788 <omprakashkumar552@gmail.com>
2025-11-15 00:16:48 +0530

    feat:update model as per production level

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,17 +1,42 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const productSchema= new mongoose.Schema({
-    name:{
-        type:String,
-        require:true
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product name is required"],
+      trim: true,
+      minlength: [3, "Name must be at least 3 characters long"],
     },
-    price:{
-        type:Number,
-        require:true
-    }
-},{
-    timestamps:true,
-})
+    price: {
+      type: Number,
+      required: [true, "Product price is required"],
+      min: [0, "price must be a positive number"],
+    },
+    category: {
+      type: String,
+      enum: ["electronics", "clothings", "home", "books"],
+      required: [true, "Category is required"],
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true}
+  }
+);
+
+productSchema.virtual("priceWithTax").get(function(){
+    return(this.price * 1.2).toFixed(2)
+});
+
+productSchema.methods.getDiscountedPrice=function(percent){
+    return this.price * (1-percent);
+}
 
 const Product = mongoose.model("Product", productSchema);
-module.exports=Product;
+module.exports = Product;
