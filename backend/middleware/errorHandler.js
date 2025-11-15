53a9f3dcb14ff6b60b704b85027f30018b2d12f0
omprakash8788 <omprakashkumar552@gmail.com>
2025-11-15 17:37:46 +0530

    feat:updating global error handker

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,8 +1,8 @@
 const errorHandler = (err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
+  const statusCode =res.statusCode !==200 ? res.statusCode:500;
   res.status(statusCode).json({
     success: false,
-    message: err.message || "Internal Server Error",
+    message: err.message,
     stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
   });
 };
--- backend/middleware/notFound.js
+++ backend/middleware/notFound.js
@@ -3,5 +3,6 @@ const notFound = (req, res, next) => {
     success: false,
     message: `Route ${req.originalUrl} not found`,
   });
+  
 };
 module.exports = notFound;
