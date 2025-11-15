f8dd88252e542f33b5ed42c7f8d1eddcf4cf6939
omprakash8788 <omprakashkumar552@gmail.com>
2025-11-15 21:45:38 +0530

    feat:restrictTo - block entry if they have no token

---
+++ backend/middleware/restrictTo.js
@@ -0,0 +1,13 @@
+const AppError = require("../utils/appError");
+
+const restrictTo = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action", 403)
+      );
+    }
+    next();
+  };
+};
+module.exports = restrictTo;
