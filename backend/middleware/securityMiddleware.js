4cfc97452c058f1efa9a47b0f08f61d93c009cb9
omprakash8788 <omprakashkumar552@gmail.com>
2025-11-15 17:56:41 +0530

    feat:adding security middleware

---
+++ backend/middleware/securityMiddleware.js
@@ -0,0 +1,10 @@
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const cors = require("cors");
+const applySecurityMiddleware = (app) => {
+  app.use(helmet());
+  app.use(mongoSanitize());
+  app.use(cors());
+};
+
+module.exports = applySecurityMiddleware;
