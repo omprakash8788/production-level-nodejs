ae8ba4b5ffbc8514416a8c86bd2fb082eec82b89
omprakash8788 <omprakashkumar552@gmail.com>
2025-11-15 21:29:49 +0530

    feat:adding route and updating app.js

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,24 @@
 const express = require("express");
 
 const app = express();
-
+ const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const errorHandler = require("./middleware/errorHandler");
+ const cookieParser = require("cookie-parser");
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cookieParser());
 
 // app.use('/api/products', productRoutes)
 // Routes
 
-
+app.use("/api/v1/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use((req, res, next) => {
   const AppError = require("./utils/appError");
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
 
 // // Simple route
 // app.get("/", (req, res) => {
