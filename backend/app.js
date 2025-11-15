67f551861ac48a51f41b3b9501d0a8dd50a77eea
omprakash8788 <omprakashkumar552@gmail.com>
2025-11-15 16:34:44 +0530

    feat:updating routes as per production level

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,31 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
-const productRoutes = require("./routes/productRoutes")
-const errorHandler=require("./middleware/errorHandler")
+const productRoutes = require("./routes/productRoutes");
+const errorHandler = require("./middleware/errorHandler");
 // Middleware to parse JSON
 app.use(express.json());
 
-app.use('/api/products', productRoutes)
-
-// Simple route
-app.get("/", (req, res)=>{
-    res.send("API is running")
+// app.use('/api/products', productRoutes)
+// Routes 
+app.use("/api/products", productRoutes);
+app.all("*", (req, res, next) => {
+  const AppError = require("./utils/appError");
+  next(
+    new AppError(
+      `Can't find
+ ${req.originalUrl} on this server`,
+      404
+    )
+  );
 });
 
-//Global error handler 
-app.use(errorHandler)
-module.exports= app;
+// Simple route
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
+//Global error handler
+app.use(errorHandler);
+module.exports = app;
